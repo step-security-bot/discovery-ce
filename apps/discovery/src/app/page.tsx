b4cfc182b5f040e5bfe30659720a925eb7da435b
Mohamed Ali <m.ali@0bytes.io>
2024-08-05 15:05:34 +0300

    chore(discovery): initialize frontend application
    Initialize the frontend application for Project Discovery, setting up the basic structure and configuration for a Next.js application using Nx.
    
    This includes:
    
    - Tailwind CSS integration for styling
    - ESLint configuration for code quality
    - Jest configuration for testing
    - Basic app layout and a sample page
    - API route example

---
+++ apps/discovery/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Index() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-slate-600">
+      <h1 className="text-6xl text-white">Hello World</h1>
+    </div>
+  );
+}
